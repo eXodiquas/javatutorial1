62213e367880252e95c381b548811aa408e659e1
Timo 'eXodiquas' Netzer <exodiquas@gmail.com>
2020-04-09 12:57:11 +0200

    Schleifen

---
+++ Schleifen.java
@@ -0,0 +1,28 @@
+class Schleifen {
+
+    public static void main(String[] args) {
+        //Do-While Schleife
+        int i = 1;
+
+        do{
+            System.out.println(i);
+            i += 1;
+        }
+        while(i <= 5);
+
+        //While Schleife
+        int j = 1;
+
+        while(j <= 5) {
+            System.out.println(j);
+            j += 1;
+        }
+
+        //For Schleife
+        for(int k = 1; k <= 5; k += 1) {
+            System.out.println(k);
+        }
+
+    }
+
+}
