1121ff73e3180840f1719d7b39085e57382dce64
Timo 'eXodiquas' Netzer <exodiquas@gmail.com>
2020-04-09 12:15:53 +0200

    Typumwandlung

---
+++ Umwandlung.java
@@ -0,0 +1,21 @@
+class Umwandlung {
+
+    public static void main(String[] args) {
+        /*
+        1 Bit           = 0 oder 1
+        1 Byte          = 8 Bit
+        1 Short         = 16 Bit
+        1 Int           = 32 Bit
+        1 Long          = 64 Bit
+        ------------------------
+        1 Float         = 32 Bit
+        1 Double        = 64 Bit 
+        */
+
+        long lon = 50000;
+        float f = lon;
+
+        System.out.println(f);
+    }
+
+}
